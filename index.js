4bca6963767cae45904b1494d2e1d346e8311731
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-11-14 17:16:38 -0500

    Add download button

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+/** @type {WebGLRenderingContext} */
 let gl = null;
 
 let generator = null;
 let quad = null;
+let capture = false;
 
 
 function draw() {
     GlUtils.resizeCanvas(gl);
-    
+
     generator.draw();
     quad.draw();
+
+    if (capture) {
+        capture = false;
+        const res = 255;
+        const pixels = new Uint8Array(res * res * 4);
+        gl.readPixels(gl.canvas.width - 265, 10, res, res, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = res;
+        canvas.height = res;
+        const data = context.createImageData(res, res);
+        data.data.set(pixels);
+        context.putImageData(data, 0, 0);
+        const imgUrl = canvas.toDataURL();
+
+        const link = document.createElement("a");
+        link.setAttribute("download", "height_map.png");
+        link.setAttribute("href", imgUrl);
+        link.click();
+        link.remove();
+        canvas.remove();
+    }
 
     requestAnimationFrame(draw);
 }
@@ -24,4 +49,8 @@ async function init() {
     await quad.init(gl);
 
     draw();
+}
+
+function downloadHeightMap() {
+    capture = true;
 }
