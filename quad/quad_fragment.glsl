412a776634dfef7d9ee4658cf6b90780e670608f
Thierry Dubois <thierry.dubois@polymtl.ca>
2020-11-16 19:05:03 -0500

    lighterWater

--- quad/quad_fragment.glsl
+++ quad/quad_fragment.glsl
@@ -13,7 +13,7 @@ varying vec3 raw_normal;
 
 const float snowLevel = 0.6;
 const float waterLevel = 0.1;
-const vec3 waterColor = vec3(0.0,0.09,0.18);
+const vec3 waterColor = vec3(0.0,0.11,0.22);
 const vec3 SEA_WATER_COLOR = vec3(0.8,0.9,0.6);
 const vec3 rockColor = vec3(0.3, 0.3, 0.3);
 const vec3 snowColor = vec3(0.8, 0.8, 0.8);
