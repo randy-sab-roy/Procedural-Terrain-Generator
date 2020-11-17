e5feae32a0cece4e24546b3185b3bbfc2270bee9
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-11-17 13:46:11 -0500

    Allow bigger resolution

--- quad/quad_vertex.glsl
+++ quad/quad_vertex.glsl
@@ -20,21 +20,28 @@ varying float height;
 
 const float waterLevel = 0.1;
 
+// https://stackoverflow.com/questions/18453302/how-do-you-pack-one-32bit-int-into-4-8bit-ints-in-glsl-webgl
+const vec4 bitEnc = vec4(1.,255.,65025.,16581375.);
+const vec4 bitDec = 1./bitEnc;
+float DecodeFloatRGBA (vec4 v) {
+    return dot(v, bitDec);
+}
+
 vec3 getNormal() {
     float tempAmp = amp;
-    if(texture2D(heightMap, uv).x <= waterLevel) {
+    if(DecodeFloatRGBA(texture2D(heightMap, uv)) <= waterLevel) {
         // water hack
         tempAmp /= 8.0;
     }
     float d = 1.0/res;
-    float p0 = texture2D(heightMap, vec2(uv.x - d, uv.y - d)).x;
-    float p1 = texture2D(heightMap, vec2(uv.x, uv.y - d)).x;
-    float p2 = texture2D(heightMap, vec2(uv.x + d, uv.y - d)).x;
-    float p3 = texture2D(heightMap, vec2(uv.x - d, uv.y)).x;
-    float p5 = texture2D(heightMap, vec2(uv.x + d, uv.y)).x;
-    float p6 = texture2D(heightMap, vec2(uv.x - d, uv.y + d)).x;
-    float p7 = texture2D(heightMap, vec2(uv.x, uv.y + d)).x;
-    float p8 = texture2D(heightMap, vec2(uv.x + d, uv.y + d)).x;
+    float p0 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x - d, uv.y - d)));
+    float p1 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x, uv.y - d)));
+    float p2 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x + d, uv.y - d)));
+    float p3 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x - d, uv.y)));
+    float p5 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x + d, uv.y)));
+    float p6 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x - d, uv.y + d)));
+    float p7 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x, uv.y + d)));
+    float p8 = DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x + d, uv.y + d)));
 
     float gx = p0 + 2.0*p1 +p2 -p6 - 2.0*p7 - p8;
     float gy = p0 - p2 + 2.0*p3 - 2.0*p5 + p7-p8;
@@ -53,7 +60,7 @@ void main() {
 
     if (uv.x != 0.0 && uv.y != 0.0 && uv.x != 1.0 && uv.y != 1.0)
     {
-        height = max(texture2D(heightMap, vec2(uv.x, uv.y)).x, waterLevel);
+        height = max(DecodeFloatRGBA(texture2D(heightMap, vec2(uv.x, uv.y))), waterLevel);
         p.z = p.z + amp * height;
         
         float delta = 1.2/res;
