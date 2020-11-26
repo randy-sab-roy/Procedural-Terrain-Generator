4ad24048aeb1748fad510b4dc95c30653a0d0336
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-11-25 21:44:09 -0500

    Cleanup generator

--- generator/generator.js
+++ generator/generator.js
@@ -6,38 +6,14 @@ class Generator {
     RES = 500;
     capture = false;
     offset = 0;
-    tempAmp = 0;
-    time = 0.0;
-    startTime;
-    newTime;
-
+    startTime = new Date();
 
     async init(gl) {
-        this.startTime = new Date();
         this.gl = gl;
         this.buffers = await this.createBuffers();
         this.program = await GlUtils.createWebGLProgramFromPath(gl, "generator/generator_vertex.glsl", "generator/generator_fragment.glsl");
         gl.useProgram(this.program);
-
-        this.locations.position = gl.getAttribLocation(this.program, "position");
-        this.locations.uv = gl.getAttribLocation(this.program, "uv");
-        this.locations.time = gl.getUniformLocation(this.program, "time");
-        this.locations.terrainOffset = gl.getUniformLocation(this.program, "terrainOffset");
-        this.locations.terrainScale = gl.getUniformLocation(this.program, "terrainScale");
-        this.locations.noise = gl.getUniformLocation(this.program, "noise");
-        this.locations.h = gl.getUniformLocation(this.program, "H");
-        this.locations.globalContrast = gl.getUniformLocation(this.program, "globalContrast");
-        this.locations.globalBrightness = gl.getUniformLocation(this.program, "globalBrightness");
-        this.locations.nOctaves = gl.getUniformLocation(this.program, "nOctaves");
-        this.locations.fAmp = gl.getUniformLocation(this.program, "fAmp");
-        this.locations.fContrast = gl.getUniformLocation(this.program, "fContrast");
-        this.locations.h1Amp = gl.getUniformLocation(this.program, "h1Amp");
-        this.locations.h1Contrast = gl.getUniformLocation(this.program, "h1Contrast");
-        this.locations.h2Amp = gl.getUniformLocation(this.program, "h2Amp");
-        this.locations.h2Contrast = gl.getUniformLocation(this.program, "h2Contrast");
-        this.locations.h3Amp = gl.getUniformLocation(this.program, "h3Amp");
-        this.locations.h3Contrast = gl.getUniformLocation(this.program, "h3Contrast");
-        this.locations.waterLevel = gl.getUniformLocation(this.program, "waterLevel");
+        this.bindLocations();
 
         gl.enable(gl.DEPTH_TEST);
     }
@@ -45,12 +21,7 @@ class Generator {
     draw() {
         const gl = this.gl;
         gl.useProgram(this.program);
-        const newRes = document.getElementById("res").value * 1.0;
-        if (this.RES != newRes) {
-            this.RES = newRes;
-            gl.bindTexture(gl.TEXTURE_2D, this.buffers.texture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.RES, this.RES, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-        }
+        this.updateResolution();
 
         gl.viewport(0, 0, this.RES, this.RES);
         gl.clearColor(0, 0, 0, 0);
@@ -64,7 +35,17 @@ class Generator {
 
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     };
-    
+
+    updateResolution() {
+        const gl = this.gl;
+        const newRes = document.getElementById("res").value * 1.0;
+        if (this.RES != newRes) {
+            this.RES = newRes;
+            gl.bindTexture(gl.TEXTURE_2D, this.buffers.texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.RES, this.RES, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+        }
+    }
+
     checkCaptureStatus() {
         if (this.capture == true) {
             this.capture = false;
@@ -75,13 +56,13 @@ class Generator {
             const bitEnc = [1, 1 / 255, 1 / 65025, 1 / 16581375];
             for (let i = 0; i < this.RES * this.RES; i++) {
                 let val = 0;
-                for(let j = 0; j < 4; j++) {
-                    val += pixels[(i*4)+j] * bitEnc[j];
+                for (let j = 0; j < 4; j++) {
+                    val += pixels[(i * 4) + j] * bitEnc[j];
                 }
-                for(let j = 0; j < 3; j++) {
-                    pixels[(i*4)+j] = val;
+                for (let j = 0; j < 3; j++) {
+                    pixels[(i * 4) + j] = val;
                 }
-                pixels[(i*4)+3] = 255;
+                pixels[(i * 4) + 3] = 255;
             }
 
 
@@ -105,8 +86,7 @@ class Generator {
 
     updateAttributesAndUniforms() {
         const gl = this.gl;
-        this.newTime = new Date();
-        this.time = (this.newTime - this.startTime) / 1000.0;
+
         // Positions
         gl.enableVertexAttribArray(this.locations.position);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.positions);
@@ -119,32 +99,30 @@ class Generator {
 
         this.offset += document.getElementById("terrainOffset").value * 1;
         gl.uniform1f(this.locations.terrainOffset, this.offset);
-        gl.uniform1f(this.locations.time, this.time);
+        gl.uniform1f(this.locations.time, (new Date() - this.startTime) / 1000.0);
         gl.uniform1f(this.locations.terrainScale, document.getElementById("terrainScale").value);
         gl.uniform1f(this.locations.h, document.getElementById("h").value);
         gl.uniform1f(this.locations.globalContrast, document.getElementById("gContrast").value);
         gl.uniform1f(this.locations.globalBrightness, document.getElementById("gBrightness").value);
         gl.uniform1i(this.locations.nOctaves, document.getElementById("octaves").value);
         gl.uniform1i(this.locations.noise, document.querySelector('input[name="noise"]:checked').value);
+        gl.uniform1f(this.locations.waterLevel, document.getElementById("waterLevel").value);
 
-        this.tempAmp = document.getElementById("showFbm").checked ? document.getElementById("fAmp").value : 0.0;
-        gl.uniform1f(this.locations.fAmp, this.tempAmp);
+        const fAmp = document.getElementById("showFbm").checked ? document.getElementById("fAmp").value : 0.0;
+        gl.uniform1f(this.locations.fAmp, fAmp);
         gl.uniform1f(this.locations.fContrast, document.getElementById("fContrast").value);
 
-        this.tempAmp = document.getElementById("showH1").checked ? document.getElementById("h1Amp").value : 0.0;
-        gl.uniform1f(this.locations.h1Amp, this.tempAmp);
+        const h1amp = document.getElementById("showH1").checked ? document.getElementById("h1Amp").value : 0.0;
+        gl.uniform1f(this.locations.h1Amp, h1amp);
         gl.uniform1f(this.locations.h1Contrast, document.getElementById("h1Contrast").value);
 
-
-        this.tempAmp = document.getElementById("showH2").checked ? document.getElementById("h2Amp").value : 0.0;
-        gl.uniform1f(this.locations.h2Amp, this.tempAmp);
+        const h2amp = document.getElementById("showH2").checked ? document.getElementById("h2Amp").value : 0.0;
+        gl.uniform1f(this.locations.h2Amp, h2amp);
         gl.uniform1f(this.locations.h2Contrast, document.getElementById("h2Contrast").value);
 
-        this.tempAmp = document.getElementById("showH3").checked ? document.getElementById("h3Amp").value : 0.0;
-        gl.uniform1f(this.locations.h3Amp, this.tempAmp);
+        const h3amp = document.getElementById("showH3").checked ? document.getElementById("h3Amp").value : 0.0;
+        gl.uniform1f(this.locations.h3Amp, h3amp);
         gl.uniform1f(this.locations.h3Contrast, document.getElementById("h3Contrast").value);
-        //
-        gl.uniform1f(this.locations.waterLevel, document.getElementById("waterLevel").value);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffers.elements);
     };
@@ -185,6 +163,28 @@ class Generator {
             frame: fb,
             texture: tex
         }
+    }
+
+    bindLocations() {
+        this.locations.position = gl.getAttribLocation(this.program, "position");
+        this.locations.uv = gl.getAttribLocation(this.program, "uv");
+        this.locations.time = gl.getUniformLocation(this.program, "time");
+        this.locations.terrainOffset = gl.getUniformLocation(this.program, "terrainOffset");
+        this.locations.terrainScale = gl.getUniformLocation(this.program, "terrainScale");
+        this.locations.noise = gl.getUniformLocation(this.program, "noise");
+        this.locations.h = gl.getUniformLocation(this.program, "H");
+        this.locations.globalContrast = gl.getUniformLocation(this.program, "globalContrast");
+        this.locations.globalBrightness = gl.getUniformLocation(this.program, "globalBrightness");
+        this.locations.nOctaves = gl.getUniformLocation(this.program, "nOctaves");
+        this.locations.fAmp = gl.getUniformLocation(this.program, "fAmp");
+        this.locations.fContrast = gl.getUniformLocation(this.program, "fContrast");
+        this.locations.h1Amp = gl.getUniformLocation(this.program, "h1Amp");
+        this.locations.h1Contrast = gl.getUniformLocation(this.program, "h1Contrast");
+        this.locations.h2Amp = gl.getUniformLocation(this.program, "h2Amp");
+        this.locations.h2Contrast = gl.getUniformLocation(this.program, "h2Contrast");
+        this.locations.h3Amp = gl.getUniformLocation(this.program, "h3Amp");
+        this.locations.h3Contrast = gl.getUniformLocation(this.program, "h3Contrast");
+        this.locations.waterLevel = gl.getUniformLocation(this.program, "waterLevel");
     }
 
     createGeneratorData() {
