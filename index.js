3d31d779f92c932da8c1cd75a379874d88d33916
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-11-25 21:20:48 -0500

    Cleanup index.js

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ let fps = 60;
 let genTime = 0;
 let quadTime = 0;
 
-function draw() {
+function updateFps() {
     fps = alpha * fps + (1.0 - alpha) * Math.min(1000 / (Date.now() - prevTime), 9999);
     document.getElementById("fps").textContent = Math.trunc(fps);
     document.getElementById("fpsBox").style = document.getElementById("showFps").checked ? "" : "display:none;";
     prevTime = Date.now();
+}
+
+function draw() {
+    updateFps();
 
     GlUtils.resizeCanvas(gl);
     
@@ -30,6 +34,7 @@ function draw() {
 async function init() {
     const mainCanvas = document.getElementById("drawCanvas");
     gl = GlUtils.createContext(mainCanvas);
+
     uints_for_indices = gl.getExtension("OES_element_index_uint");
 
     generator = new Generator();
